888bae9528cb12cb29f26fa044975a88641f1c5b
tecdesdev <pichou.kutekidila@gmail.com>
2015-07-14 15:56:42 +0200

    In Progress

---
+++ testNG/src/main/java/com/arrival/testNG/arrivalGeneric/IFGenericAND.java
@@ -0,0 +1,18 @@
+package com.arrival.testNG.arrivalGeneric;
+
+/**
+ * Created by tecdesdev on 01/07/15.
+ */
+public interface IFGenericAND {
+    void clickAND();
+    void doppleClickAND();
+    void pressAND();
+    void longPressAND();
+    void swipeRightAND();
+    void swipeLeftAND();
+    void scrollUpAND();
+    void scrollDownAND();
+    void zoomInAND();
+    void zoomOutAND();
+    void takeScreenShotAND();
+}
---
+++ testNG/src/main/java/com/arrival/testNG/arrivalGeneric/IFGenericIOS.java
@@ -0,0 +1,18 @@
+package com.arrival.testNG.arrivalGeneric;
+
+/**
+ * Created by tecdesdev on 06/07/15.
+ */
+public interface IFGenericIOS {
+    void clickIOS();
+    void doppleClickIOS();
+    void pressIOS();
+    void longPressIOS();
+    void swipeRightIOS();
+    void swipeLeftIOS();
+    void scrollUpIOS();
+    void scrollDownIOS();
+    void zoomInIOS();
+    void zoomOutIOS();
+    void takeScreenShotIOS();
+}
