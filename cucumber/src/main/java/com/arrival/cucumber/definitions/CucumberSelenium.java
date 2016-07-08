ec0252e54d8498305338e1b5fb1cbcac9c1c9c3c
AaronPichouKutekidila <pichou.kutekidila@gmail.com>
2016-07-08 12:51:59 +0200

    In Progress

--- cucumber/src/main/java/com/arrival/cucumber/definitions/CucumberSelenium.java
+++ cucumber/src/main/java/com/arrival/cucumber/definitions/CucumberSelenium.java
@@ -1,6 +1,5 @@
 package com.arrival.cucumber.definitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 
 /**
@@ -8,15 +7,14 @@ import cucumber.api.java.en.Given;
  */
 public class CucumberSelenium {
 
-    @Given("^openURL url \"([^\"]*)\"$")
+    @Given("^openURL @url: \"([^\"]*)\"$")
     public void openurl_url(String arg1)   {
         // Write code here that turns the phrase above into concrete actions
-        System.out.format("Test: %n\n", arg1);
-
+        System.out.println("Test: " +  arg1);
     }
 
     @Given("^I have (\\d+) cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        System.out.format("Cukes: %n\n", cukes);
+        System.out.println("Test: " +  cukes);
     }
 }
--- cucumber/src/main/java/com/arrival/cucumber/feature/openURL.feature
+++ cucumber/src/main/java/com/arrival/cucumber/feature/openURL.feature
@@ -1,6 +1,6 @@
 Feature: CucumberSelenium
   Scenario: Open URL
-    Given openURL url "aaron"
+    Given openURL @url: "aaron"
 
   Scenario: First Test
     Given I have 1000 cukes in my belly
